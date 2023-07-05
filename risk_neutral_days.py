57000356d349c692613153af6f0a6f10518e2069
leenleenaerts <118093301+leenleenaerts@users.noreply.github.com>
2023-07-04 22:47:05 -0600

    Update risk_neutral_days.py

--- risk_neutral_days.py
+++ risk_neutral_days.py
@@ -35,14 +35,20 @@ std_dev = df.pct_change().std() * np.sqrt(365/252)
 
 # MONTE CARLO SIMULATION
 # assume normal distribution of returns
-counter = 0
+counter1 = 0
+counter2 = 0
 cur_price = df.iloc[-1]
 for i in range(trials):
     price = cur_price
-    daily_return = np.random.normal(drift / 365, std_dev, days_to_expiration) + 1
+    daily_return = np.random.normal(drift / days_to_expiration, std_dev, days_to_expiration) + 1
+    strike_met = False
     for r in daily_return:
         price *= r
+        if price >= strike_price and strike_met == False:
+            counter1 += 1
+            strike_met = True
     if price >= strike_price:
-        counter += 1
+        counter2 += 1
 
-print(counter/trials)
+print(f"The probability of being above strike at any time before expiration date is {counter1/trials * 100} %")
+print(f"The probability of being above strike on expiration date {counter2/trials * 100} %")
