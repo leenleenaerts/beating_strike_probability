19e665a28619859671b215d47a8070c2854be4fa
leenleenaerts <118093301+leenleenaerts@users.noreply.github.com>
2023-07-04 22:45:11 -0600

    Update risk_neutral_trading_days.py

--- risk_neutral_trading_days.py
+++ risk_neutral_trading_days.py
@@ -1,3 +1,35 @@
+import numpy as np
+import pandas_datareader.data as web
+from datetime import datetime, timedelta
+import yfinance as yfin
+
+yfin.pdr_override()
+
+# PARAMETERS TO SET
+discount_rate = 0.0525
+arithmic_div_yield = 0.005
+stock = "AAPL"
+days_to_expiration = 365
+trading_days_to_expiration = round(days_to_expiration * 252/365)
+strike_price = 200
+trials = 10000
+
+start = datetime.today() - timedelta(days = 365*7.5)
+end = datetime.today()
+
+# calculate true yield
+cash_price = 1 - discount_rate * 30/360
+true_yield = 365/30*np.log(1+0.004375/cash_price)
+
+# calculate continuous dividend yield
+cont_div_yield = np.log(1 + arithmic_div_yield)
+
+# calculate risk neutral drift
+drift = true_yield - cont_div_yield
+
+# read stock data
+df = web.DataReader(stock, start, end)["Adj Close"]
+
 # calculate standard deviation
 std_dev = df.pct_change().std() * np.sqrt(365/252)
 
