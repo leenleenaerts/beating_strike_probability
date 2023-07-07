16a4fd0ecabc8825c391d8e5c48665935a614bca
leenleenaerts <118093301+leenleenaerts@users.noreply.github.com>
2023-07-07 14:36:21 -0600

    Update continuous_monte_carlo.py

--- continuous_monte_carlo.py
+++ continuous_monte_carlo.py
@@ -13,47 +13,51 @@ expiration_date = "07/14/2023 23:59:59"
 K = 32500
 
 
-trials = 5000
-now = datetime.now()
-today = datetime.now()
-dif = (datetime.strptime(expiration_date, "%m/%d/%Y %H:%M:%S") - now)
-days_to_expiration = dif.days + dif.seconds / 24 / 3600
-start = today - timedelta(days = 365*5)
-end = datetime.today()
+for j in [7,14,30,90,365,365*5, 365*10]:
 
-# calculate true yield
-cash_price = 1 - discount_rate * days_to_expiration/360
-true_yield = 365/days_to_expiration*np.log(1+0.004375/cash_price)
+    trials = 500
+    now = datetime.now()
+    today = datetime.now()
+    dif = (datetime.strptime(expiration_date, "%m/%d/%Y %H:%M:%S") - now)
+    days_to_expiration = dif.days + dif.seconds / 24 / 3600
+    start = today - timedelta(days = j)
+    end = datetime.today()
 
-# calculate continuous dividend yield
-cont_div_yield = np.log(1 + arithmic_div_yield)
+    # calculate true yield
+    cash_price = 1 - discount_rate * days_to_expiration/360
+    true_yield = 365/days_to_expiration*np.log(1+0.004375/cash_price)
 
-# calculate risk neutral drift
-drift = true_yield - cont_div_yield
+    # calculate continuous dividend yield
+    cont_div_yield = np.log(1 + arithmic_div_yield)
 
-# read stock data
-df = web.DataReader(stock, start, end)["Adj Close"]
+    # calculate risk neutral drift
+    drift = true_yield - cont_div_yield
 
-# calculate standard deviation
-std_dev = df.pct_change().std() * np.sqrt(252/3650000)
+    # read stock data
+    df = web.DataReader(stock, start, end)["Adj Close"]
 
-# MONTE CARLO SIMULATION
-# assume normal distribution of returns
-counter1 = 0
-counter2 = 0
-cur_price = stock_info.get_live_price(stock)
-for i in range(trials):
-    price = cur_price
-    daily_return = np.random.normal(drift / 3650000, std_dev, round(days_to_expiration*10000)) + 1
-    strike_met = False
-    for r in daily_return:
-        price *= r
-        if price >= K and strike_met == False:
-            counter1 += 1
-            strike_met = True
-    if price >= K:
-        counter2 += 1
+    # calculate standard deviation
+    std_dev = df.pct_change().std() * np.sqrt(252/3650000)
 
-print(f"current price of {stock} = ${cur_price}.")
-print(f"The probability of {stock} being above ${K} at any time before expiration date is {counter1/trials * 100} %")
-print(f"The probability of {stock} being above ${K} on expiration date {counter2/trials * 100} %")
+    # MONTE CARLO SIMULATION
+    # assume normal distribution of returns
+    counter1 = 0
+    counter2 = 0
+    cur_price = stock_info.get_live_price(stock)
+    for i in range(trials):
+        price = cur_price
+        daily_return = np.random.normal(drift / 3650000, std_dev, round(days_to_expiration*10000)) + 1
+        strike_met = False
+        for r in daily_return:
+            price *= r
+            if price >= K and strike_met == False:
+                counter1 += 1
+                strike_met = True
+        if price >= K:
+            counter2 += 1
+
+    print("\n")
+    print(f"Used data from the last {j} days")
+    print(f"current price of {stock} = ${cur_price:.2f}.")
+    print(f"The probability of {stock} being above ${K} at any time before expiration date is {counter1/trials * 100:.2f} %")
+    print(f"The probability of {stock} being above ${K} on expiration date {counter2/trials * 100:.2f} %")
